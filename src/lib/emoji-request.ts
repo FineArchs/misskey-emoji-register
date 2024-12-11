2644496d3dafb0e29b4eaa5d8301090ae32084b0
FineArchs <cicada1003@gmail.com>
2024-12-12 07:25:06 +0900

    wip

--- src/lib/emoji-request.ts
+++ src/lib/emoji-request.ts
@@ -28,7 +28,7 @@ const requiredFields: [string, (text: string, emoji: Partial<Emoji>) => (void | 
   // ["⑧",
 ];
 const numberings = requiredFields.map(v => v[0]);
-const numberingRegexp = new RegExp("[" + numberings.join("") + "]");
+const numberingRegexp = new RegExp("(" + numberings.join("|") + ")");
 
 function extractEmojiName(text: string): Emoji | null {
   const matches = text.match(/:([a-z0-9_+-]+):/i);
@@ -48,10 +48,23 @@ export function parseEmojiRequest(note: Note): (Emoji | string)[] {
     if (note.file.length <= index) return "対応する画像がありません";
     const numberingErrors = numberings.flatMap(num => {
       const count = val.split(num).length;
-      if (count === 1) return [];
-      if (count === 0) return num + "がありません";
+      if (count === 2) return [];
+      if (count === 1) return num + "がありません";
       return `${num}が${count}つあります`;
     });
     if (numberingErrors.length > 0) return numberingErrors.join("\n");
+
+    const emoji: Partial<Emoji> = {};
+
+    const fieldTexts = val.split(numberingRegexp);
+    let idx = 0;
+    for (const i in fieldTexts) {
+      
+    while (true) {
+
+    const fieldMatches = [...val.matchAll(new RegExp(
+    const matchedNumberings = [...val.matchAll(numberingRegexp)].map(match => match[0]);
+    matchedNumberings.shift();
+    const 
 
     requiredFields.
